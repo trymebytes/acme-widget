a4068efdc6954ae7d2adaab8375cd2a24511c968
Tosin Oguntuyi <tcubed03@gmail.com>
2025-06-04 23:55:53 +0100

    Add basket test

---
+++ tests/BasketTest.php
@@ -0,0 +1,46 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use AcmeWidget\Basket;
+use AcmeWidget\ProductCatalogue;
+use AcmeWidget\DeliveryFeeRule;
+use AcmeWidget\Offer;
+
+class BasketTest extends TestCase
+{
+    private function basket()
+    {
+        return new Basket( 
+            new ProductCatalogue(), 
+            new DeliveryFeeRule(), 
+            new Offer()
+        );
+    }
+    public function test_total_under_50()
+    {
+        $basket =$this->basket();
+        $basket->add('B01'); 
+        $basket->add('G01');
+        
+        $this->assertEquals(37.85, $basket->total());
+    }
+
+    public function test_total_under_90()
+    {
+        $basket =$this->basket();
+        $basket->add('R01'); 
+        $basket->add('G01');
+        
+        $this->assertEquals(60.85, $basket->total());
+    }
+
+    public function test_total_greater_than_90()
+    {
+        $basket =$this->basket();
+        $basket->add('R01'); 
+        $basket->add('G01');
+        $basket->add('G01');
+        $basket->add('G01');
+
+        $this->assertEquals(107.8, $basket->total());
+    }
+}
